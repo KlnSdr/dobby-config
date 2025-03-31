d410de6dd5c7b5948efec50c63206eaee05c6d87
KlnSdr <monsieurk1209@gmail.com>
2025-03-31 20:31:11 +0200

    initial commit

---
+++ src/dobby/Dobby.java
@@ -0,0 +1,9 @@
+package dobby;
+
+public class Dobby {
+    // This is a placeholder for the Dobby class.
+    // will be overwritten when building with dobby as a dependency
+    public static Class<?> getMainClass() {
+        return null;
+    }
+}
